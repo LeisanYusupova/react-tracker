4e34d5f225a09273c0b31840d91f1f60c4b6bd3f
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-13 10:58:47 +0300

    fix

--- src/Pages/homePage/HomePage.jsx
+++ src/Pages/homePage/HomePage.jsx
@@ -1,8 +1,8 @@
 
 
 export const HomePage = () => {
-
-        window.onload = function() {
+    document.addEventListener("DOMContentLoaded", function() {
+        window.onload = function () {
             window.YaAuthSuggest.init({
                     client_id: '202f06d8b9824ee680dd9201edf6bf64',
                     response_type: 'token',
@@ -16,18 +16,19 @@ export const HomePage = () => {
                     buttonSize: 'm',
                     buttonBorderRadius: 0
                 })
-                .then(function(result) {
+                .then(function (result) {
                     return result.handler()
                 })
-                .then(function(data) {
+                .then(function (data) {
                     console.log('Сообщение с токеном: ', data);
                     document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     console.log('Что-то пошло не так: ', error);
                     document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
                 });
-        };
+        }
+    })
 
     return (
         <div>
