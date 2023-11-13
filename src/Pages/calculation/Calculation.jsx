31b30d0a170ba4138826fcdb757468022a656f0c
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-13 20:54:15 +0300

    fix

--- src/Pages/calculation/Calculation.jsx
+++ src/Pages/calculation/Calculation.jsx
@@ -1,29 +1,7 @@
+
+import React, { useEffect, useState } from 'react';
+
 export const Calculation  = () => {
-    const requestOptions = {
-        method: 'GET',
-        mode: 'no-cors',
-        headers: {
-            'Authorization': 'OAuth y0_AgAAAAAjDSwQAArFuQAAAADxFlH4pT5rM82wR9aup7MuB6eVNUKtaYg',
-            'Content-Type': 'application/json',
-            'X-Org-ID': '7031733',
-        },
-    };
-
-    fetch('https://api.tracker.yandex.net/v2/worklog?createdBy=leisanyagmurova&start=from:2023-11-01T00:00:00.008&start=to:2023-11-30T00:00:00.00', requestOptions)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            // Обрабатываем полученные данные
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('Request failed:', error);
-        });
-
 
     return (
         <>
