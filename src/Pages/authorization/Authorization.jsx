5c37fca6da3c1d005c80e01f5ed486f59dd2a90a
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-13 11:25:48 +0300

    fix

--- src/Pages/authorization/Authorization.jsx
+++ src/Pages/authorization/Authorization.jsx
@@ -1,18 +1,16 @@
 export const Authorization = () => {
     return (
         <>
-            <div>
-                Redirect page
-            </div>
-            {/*<script>*/}
-            {/*    document.addEventListener("DOMContentLoaded", function()*/}
-            {/*    {*/}
-            {/*    window.onload = function() {*/}
-            {/*    window.YaSendSuggestToken("https://react-tracker-ochre.vercel.app", {*/}
-            {/*        "kek": true*/}
-            {/*    })*/}
-            {/*}})*/}
-            {/*</script>*/}
+
+            <script>
+                document.addEventListener("DOMContentLoaded", function()
+                {
+                window.onload = function() {
+                window.YaSendSuggestToken("https://react-tracker-ochre.vercel.app", {
+                    "kek": true
+                })
+            }})
+            </script>
         </>
     );
 };
