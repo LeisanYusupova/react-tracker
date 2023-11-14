90ed943fc14c05029d0b6a3fb838907f49694e24
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-15 00:48:36 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -31,8 +31,5 @@ window.onload = () => {
                 console.log(result, data);
             })
             .catch((error) => console.log("Что-то пошло не так: ", error));
-    };
-    document.getElementById("button").onclick = () => {
-        // TODO button
-    //};
-};
+    }
+
