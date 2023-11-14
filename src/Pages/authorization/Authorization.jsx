4aead7a3801fc7dfc73eaf3dbe9946ea99b530a8
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-14 22:40:50 +0300

    fix

--- src/Pages/authorization/Authorization.jsx
+++ src/Pages/authorization/Authorization.jsx
@@ -1,17 +1,5 @@
 export const Authorization = () => {
-
-    return (
-        <>
-
-            <script>
-                document.addEventListener("DOMContentLoaded", function()
-                {
-                window.onload = function() {
-                window.YaSendSuggestToken("https://react-tracker-ochre.vercel.app", {
-                    "kek": true
-                })
-            }})
-            </script>
-        </>
-    );
+    window.onload = () => {
+        YaSendSuggestToken('https://react-tracker-ochre.vercel.app/');
+    };
 };
--- src/Pages/homePage/HomePage.jsx
+++ src/Pages/homePage/HomePage.jsx
@@ -1,39 +1,45 @@
 
 export const HomePage = () => {
-    console.log('test');
-    document.addEventListener("DOMContentLoaded", function() {
-        window.onload = function () {
-            window.YaAuthSuggest.init({
+    // const authorize = ({
+    //                        default_avatar_id: defaultAvatarId,
+    //                        display_name: displayName,
+    //                    }) => {
+    //     const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+    //     const nameHtml = `<div class="name">${displayName}</div>`;
+    //
+    //     document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+    // };
+
+// Делаем запрос за инфой о пользователе.
+//     const fetchYandexData = (token) =>
+//         fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+//             (res) => res.json()
+//         );
+
+    window.onload = () => {
+        document.getElementById("suggest").onclick = () => {
+            YaAuthSuggest.init({
                     client_id: '202f06d8b9824ee680dd9201edf6bf64',
                     response_type: 'token',
                     redirect_uri: 'https://react-tracker-ochre.vercel.app/authorization'
                 },
-                'https://react-tracker-ochre.vercel.app/', {
-                    view: 'button',
-                    parentId: 'container',
-                    buttonView: 'main',
-                    buttonTheme: 'light',
-                    buttonSize: 'm',
-                    buttonBorderRadius: 0
+                '--https://react-tracker-ochre.vercel.app/--'
+            )
+                .then(({ handler }) => handler())
+                .then(async (data) => {
+                    console.log(data);
                 })
-                .then(function (result) {
-                    return result.handler()
-                })
-                .then(function (data) {
-                    console.log('Сообщение с токеном: ', data);
-                    localStorage.setItem('onderToken', data);
-                    document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
-                })
-                .catch(function (error) {
-                    console.log('Что-то пошло не так: ', error);
-                    document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
-                });
-        }
-    })
-
+                .catch((error) => console.log("Что-то пошло не так: ", error));
+        };
+        document.getElementById("button").onclick = () => {
+        };
+    };
     return (
-        <div>
-            <div id="container">Authorization</div>
-        </div>
-    );
+        <>
+            <div className="buttons">
+                <button id="suggest">Саджест</button>
+                <button id="button">Кнопка</button>
+            </div>
+        </>
+    )
 };
