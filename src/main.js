4af7c72fd7d64e499ecc07fd5a8f58de1a73bbbc
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-15 10:40:33 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 // Авторизуем пользователя, используя инфу о нем.
 
-const authorize = ({
-                       default_avatar_id: defaultAvatarId,
-                       display_name: displayName,
-                   }) => {
-    const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
-    const nameHtml = `<div class="name">${displayName}</div>`;
-
-    document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
-};
+// const authorize = ({
+//                        default_avatar_id: defaultAvatarId,
+//                        display_name: displayName,
+//                    }) => {
+//     const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+//     const nameHtml = `<div class="name">${displayName}</div>`;
+//
+//     document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+// };
 
 // Делаем запрос за инфой о пользователе.
-const fetchYandexData = (token) =>
-    fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
-        (res) => res.json()
-    );
+// const fetchYandexData = (token) =>
+//     fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+//         (res) => res.json()
+//     );
 
 window.onload = () => {
     document.getElementById("suggest").onclick = () => {
@@ -28,9 +28,9 @@ window.onload = () => {
         )
             .then(({ handler }) => handler())
             .then(async (data) => {
-                const result = await fetchYandexData(data.access_token);
-                authorize(result);
-                console.log(result, data);
+                //const result = await fetchYandexData(data.access_token);
+                //authorize(result);
+                console.log(data);
                 window.sessionStorage.setItem('token_session', data.access_token);
                 document.getElementById('button').disabled = false;
                 document.getElementById('button').onclick = () => {
