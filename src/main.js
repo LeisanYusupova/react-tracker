024b9b9404c1a6b0044245ff657e0d020cd13087
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-15 08:42:51 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,36 +1,38 @@
 // Авторизуем пользователя, используя инфу о нем.
-// const authorize = ({
-//                        default_avatar_id: defaultAvatarId,
-//                        display_name: displayName,
-//                    }) => {
-//     const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
-//     const nameHtml = `<div class="name">${displayName}</div>`;
-//
-//     document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
-// };
+const authorize = ({
+                       default_avatar_id: defaultAvatarId,
+                       display_name: displayName,
+                   }) => {
+    const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+    const nameHtml = `<div class="name">${displayName}</div>`;
+
+    document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
 
 // Делаем запрос за инфой о пользователе.
-// const fetchYandexData = (token) =>
-//     fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
-//         (res) => res.json()
-//     );
+const fetchYandexData = (token) =>
+    fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+        (res) => res.json()
+    );
 
 window.onload = () => {
     document.getElementById("suggest").onclick = () => {
         YaAuthSuggest.init({
-                client_id: '202f06d8b9824ee680dd9201edf6bf64',
+                client_id: '1620b5a01e8d43e88d43df26f7141ed2',
                 response_type: 'token',
-                redirect_uri: 'https://react-tracker-ochre.vercel.app/authorization.html'
+                redirect_uri: 'https://oauth-master-class-pi.vercel.app/token.html'
             },
-            '--https://react-tracker-ochre.vercel.app/--'
+            '--https://oauth-master-class-pi.vercel.app/--'
         )
-            .then(({handler}) => handler())
+            .then(({ handler }) => handler())
             .then(async (data) => {
-                //const result = await fetchYandexData(data.access_token);
-                //authorize(result);
+                const result = await fetchYandexData(data.access_token);
+                authorize(result);
                 console.log(result, data);
             })
             .catch((error) => console.log("Что-то пошло не так: ", error));
-    }
-}
-
+    };
+    document.getElementById("button").onclick = () => {
+        // TODO button
+    };
+};
