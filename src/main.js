6aedce66b854c1f0cf13c75960c41ed92b0fdd03
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-15 08:45:38 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // Авторизуем пользователя, используя инфу о нем.
-const authorize = ({
-                       default_avatar_id: defaultAvatarId,
-                       display_name: displayName,
-                   }) => {
-    const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
-    const nameHtml = `<div class="name">${displayName}</div>`;
-
-    document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
-};
+// const authorize = ({
+//                        default_avatar_id: defaultAvatarId,
+//                        display_name: displayName,
+//                    }) => {
+//     const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+//     const nameHtml = `<div class="name">${displayName}</div>`;
+//
+//     document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+// };
 
 // Делаем запрос за инфой о пользователе.
-const fetchYandexData = (token) =>
-    fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
-        (res) => res.json()
-    );
+// const fetchYandexData = (token) =>
+//     fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+//         (res) => res.json()
+//     );
 
 window.onload = () => {
     document.getElementById("suggest").onclick = () => {
@@ -26,13 +26,10 @@ window.onload = () => {
         )
             .then(({ handler }) => handler())
             .then(async (data) => {
-                const result = await fetchYandexData(data.access_token);
-                authorize(result);
-                console.log(result, data);
+                // const result = await fetchYandexData(data.access_token);
+                // authorize(result);
+                console.log(data);
             })
             .catch((error) => console.log("Что-то пошло не так: ", error));
-    };
-    document.getElementById("button").onclick = () => {
-        // TODO button
     };
 };
