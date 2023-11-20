2376907813ca26f67ea60416f21ded0ba2e68a5f
Leisan Iusupova <leisan_11_@mail.ru>
2023-11-21 00:39:49 +0300

    fix

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,30 +1,32 @@
-import { useEffect } from "react";
-
 export const Auth = () => {
-    useEffect(() => {
+    const handleAuthButtonClick = () => {
         const script = document.createElement('script');
         script.src = 'https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js';
         document.body.appendChild(script);
-        
+
         script.onload = () => {
             window.YaAuthSuggest.init({
-              client_id: '202f06d8b9824ee680dd9201edf6bf64',
-              response_type: 'token',
-              redirect_uri: 'https://react-tracker-ty5h.vercel.app/'
+                client_id: '202f06d8b9824ee680dd9201edf6bf64',
+                response_type: 'token',
+                redirect_uri: 'https://react-tracker-ty5h.vercel.app/'
             }, '--https://react-tracker-ty5h.vercel.app/--')
-            .then(({ handler }) => handler())
-            .then(async (data) => {
-                console.log(data);
-        });
-      }
-      return () => {
-          document.body.removeChild(script);
-      }
-    }, []);
+                .then(({ handler }) => handler())
+                .then(async (data) => {
+                    console.log(data);
+                });
+        };
 
-  return (
-      <div>
-        Auth   
-      </div>
-  )
+        // Удалить скрипт после выполнения
+        script.onload = () => {
+            document.body.removeChild(script);
+        };
+    };
+
+    return (
+        <div>
+            <button onClick={handleAuthButtonClick}>
+                Auth
+            </button>
+        </div>
+    );
 };
