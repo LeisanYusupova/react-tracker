5a82aa5c871bc4cdafba189378480e23c4bc5724
Leisan Iusupova <leisan_11_@mail.ru>
2024-01-26 19:00:39 +0300

    fix

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,25 +1,15 @@
 export const Auth = () => {
     const handleAuthButtonClick = () => {
-        const script = document.createElement('script');
-        script.src = 'https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-with-polyfills-latest.js';
-        document.body.appendChild(script);
-
-        script.onload = () => {
             window.YaAuthSuggest.init({
                 client_id: '202f06d8b9824ee680dd9201edf6bf64',
                 response_type: 'token',
-                redirect_uri: 'https://react-tracker-ty5h.vercel.app/'
+                redirect_uri: 'https://react-tracker-ty5h.vercel.app/token.html'
             }, '--https://react-tracker-ty5h.vercel.app/--')
                 .then(({ handler }) => handler())
                 .then(async (data) => {
                     console.log(data);
                 });
-        };
 
-        // Удалить скрипт после выполнения
-        script.onload = () => {
-            document.body.removeChild(script);
-        };
     };
 
     return (
