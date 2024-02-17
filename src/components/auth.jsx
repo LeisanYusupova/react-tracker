cde0eda2ebc504d64bb31a8eb012f3898eb704d0
Leisan Iusupova <leisan_11_@mail.ru>
2024-02-17 16:37:15 +0300

    fix

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,13 +1,20 @@
 export const Auth = () => {
     const handleAuthButtonClick = () => {
+        let authHandler;
             window.YaAuthSuggest.init({
                 client_id: '202f06d8b9824ee680dd9201edf6bf64',
                 response_type: 'token',
                 redirect_uri: 'https://react-tracker-ty5h.vercel.app/token.html'
             }, '--https://react-tracker-ty5h.vercel.app/--')
-                .then(({ handler }) => handler())
+                .then(({ handler }) => {
+                    authHandler = handler;
+                    return handler();
+                })
                 .then(async (data) => {
                     console.log(data);
+                    if (typeof authHandler === 'function') {
+                        authHandler.unmount();
+                    }
                 });
 
     };
