31e1fd2293a06f18d1fd2aed7f4ebf6de1ee2fc8
Leisan Iusupova <leisan_11_@mail.ru>
2024-02-18 11:10:59 +0300

    fix

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -8,8 +8,8 @@ export const Auth = () => {
             window.YaAuthSuggest.init({
                 client_id: '59f0959964be487a84f13f7b64a4821d',
                 response_type: 'token',
-                redirect_uri: 'https://react-tracker-ty5h.vercel.app/token.html'
-            }, 'https://react-tracker-ty5h.vercel.app/')
+                redirect_uri: 'https://react-tracker-leisan-yusupovas-projects.vercel.app/token.html'
+            }, '--https://react-tracker-leisan-yusupovas-projects.vercel.app/--')
                 .then(({ handler }) => {
                     authHandler = handler;
                     return handler();
