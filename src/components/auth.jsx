f8490d117899988cc1e4dfeeeea59051ff8b3934
Leisan Iusupova <leisan_11_@mail.ru>
2024-02-21 09:14:15 +0300

    fix

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,23 +1,35 @@
 export const Auth = () => {
-    // let authHandler;
+    let authHandler;
+    let isInitInProgress = false;
+
     const handleAuthButtonClick = () => {
-        // if (authHandler && typeof authHandler.unmount === 'function') {
-        //     authHandler.unmount();
-        // }
+        if (authHandler && typeof authHandler.unmount === 'function') {
+            authHandler.unmount();
+        }
 
-            window.YaAuthSuggest.init({
-                client_id: '59f0959964be487a84f13f7b64a4821d',
-                response_type: 'token',
-                redirect_uri: 'https://react-tracker-leisan-yusupovas-projects.vercel.app/token.html'
-            }, '--https://react-tracker-leisan-yusupovas-projects.vercel.app/--')
-                .then(({ handler }) => {
-                    // authHandler = handler;
-                    return handler();
-                })
-                .then(async (data) => {
-                    console.log(data);
-                });
+        if (isInitInProgress) {
+            return; // Если инициализация уже запущена, выходим из функции
+        }
 
+        isInitInProgress = true;
+
+        window.YaAuthSuggest.init({
+            client_id: '59f0959964be487a84f13f7b64a4821d',
+            response_type: 'token',
+            redirect_uri: 'https://react-tracker-leisan-yusupovas-projects.vercel.app/token.html'
+        }, '--https://react-tracker-leisan-yusupovas-projects.vercel.app/--')
+            .then(({ handler }) => {
+                authHandler = handler;
+                isInitInProgress = false; // Сбрасываем флаг, так как инициализация завершена
+                return handler();
+            })
+            .then(async (data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error during initialization:', error);
+                isInitInProgress = false; // Обработка ошибки, сбрасываем флаг
+            });
     };
 
     return (
