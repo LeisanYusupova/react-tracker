ae657606d973b802e6151d142a52176cc1bc5696
Leisan Iusupova <leisan_11_@mail.ru>
2024-02-22 16:33:15 +0300

    fix

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,15 +1,24 @@
+import {useState} from 'react';
 export const Auth = () => {
+    const [authInProgress, setAuthInProgress] = useState(false);
     const handleAuthButtonClick = () => {
-        window.YaAuthSuggest.init({
-            client_id: '59f0959964be487a84f13f7b64a4821d',
-            response_type: 'token',
-            redirect_uri: 'https://react-tracker-leisan-yusupovas-projects.vercel.app/token.html'
-        }, '--https://react-tracker-leisan-yusupovas-projects.vercel.app/--')
-            .then(({handler}) => handler())
-            .then(async (data) => {
-                console.log(data);
-            })
-            .catch((error) => console.log('Что-то пошло не так: ', error));
+        if (!authInProgress) {
+            setAuthInProgress(true);
+            window.YaAuthSuggest.init({
+                client_id: '59f0959964be487a84f13f7b64a4821d',
+                response_type: 'token',
+                redirect_uri: 'https://react-tracker-leisan-yusupovas-projects.vercel.app/token.html'
+            }, '--https://react-tracker-leisan-yusupovas-projects.vercel.app/--')
+                .then(({ handler }) => handler())
+                .then(async (data) => {
+                    console.log(data);
+                    setAuthInProgress(false);
+                })
+                .catch((error) => {
+                    console.log('Что-то пошло не так: ', error);
+                    setAuthInProgress(false);
+                });
+        }
     }
 
     return (
